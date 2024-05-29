141acdf8cc58659e2015b36bf3e1495f58353995
daniilseliverstov <selivrstov91@internet.ru>
2024-05-29 19:07:08 +0300

    Update Homework 3.1.py

--- Homework 3.1.py
+++ Homework 3.1.py
@@ -1,7 +1,7 @@
 def single_root_words (root_word, *other_words):
     same_words = []
     for n in other_words:
-        if root_word in n or n in root_word:
+        if root_word.lower() in n.lower() or n.lower() in root_word.lower():
             same_words.append(n)
 
     return same_words
