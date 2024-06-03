4b544db586eb4d3bd046c31bde4ea64ef070bc29
daniilseliverstov <selivrstov91@internet.ru>
2024-06-03 22:41:57 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,13 @@
+a = (int(input('введите число от 3 до 20 ')))
+key = []
+for b in range(1, 20):
+    for c in range(1, 20):
+        if a % (b + c) == 0  and b < c:
+            key.append(b)
+            key.append(c)
+print(a, ' - ', key)
+
+
+
+
+
