1b0ab76c78b5eaddf5072d5bee58b7f77ce77a81
daniilseliverstov <selivrstov91@internet.ru>
2024-07-13 09:20:05 +0300

    Домашнее задание по теме "Доступ к свойствам родителя. Переопределение свойств."

--- Module 7/Module 7.5.py
+++ Module 7/Module 7.5.py
@@ -6,11 +6,11 @@ print(directory)
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(file)
-        filetime = os.path.getmtime(file)
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(file)
-        parent_dir = os.path.dirname(directory)
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(root)
 
-        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт,'
-              f' Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+        print(f'Обнаружен файл: {file}\n Путь: {filepath}\n Размер: {filesize} байт\n'
+              f' Время изменения: {formatted_time}\n Родительская директория: {parent_dir}\n\n')
