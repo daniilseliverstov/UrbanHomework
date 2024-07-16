fe46cbd10300fcf5ff3be3d690026557ad91d067
daniilseliverstov <selivrstov91@internet.ru>
2024-07-16 06:58:15 +0300

    Дополнительное практическое задание по модулю*

--- Module 6/module6hard.py
+++ Module 6/module6hard.py
@@ -4,9 +4,12 @@ import math
 class Figure:
     sides_count = 0
 
-    def __init__(self, color, *sides: int, filled=True):
-        self.__sides = list(sides)
-        self.__color = list(color)
+    def __init__(self, color: tuple, *sides: int, filled: bool = True):
+        if len(sides) != self.sides_count:
+            self.__sides = [1*self.sides_count]
+        else:
+            self.__sides = [i for i in sides]
+        self.__color = color
         self.filled = filled
 
     def get_color(self):
@@ -25,11 +28,10 @@ class Figure:
             self.__color = (r, g, b)
 
     def __is_valid_sides(self, *sides):
-        for element in sides:
-            if element > 0 and isinstance(element, int) and sides == self.__sides:
-                return True
-            else:
-                return False
+        cond1 = len(sides) == 1
+        cond2 = all([isinstance(side, int) and side > 0 for side in sides])
+        return cond1 and cond2
+
 
     def get_sides(self):
         return self.__sides
@@ -46,16 +48,19 @@ class Figure:
 class Circle(Figure):
     sides_count = 1
 
-    def __radius(self):  # Не атрибут а метод?
+    def get_radius(self):  # Не атрибут а метод?
         r = self.__len__() / (2 * math.pi)
         return r
 
     def get_square(self):
-        return (math.pi * self.__radius()) ** 2
+        return (math.pi * self.get_radius()) ** 2
 
 
 class Triangle(Figure):
     sides_count = 3
+
+    def __init__(self, color: tuple, *sides):
+        super().__init__(color, *sides)
 
     def __calculate_height(self):
         a, b, c = self.get_sides()
@@ -68,12 +73,28 @@ class Triangle(Figure):
         __height = self.__calculate_height
         return __height
 
+    def get_square(self):
+        a, b, c = self.get_sides()
+        p = sum(self.get_sides()) / 2
+        s = (p * (p - self.get_sides()[a])) * (p - self.get_sides()[b]) * (p - self.get_sides()[c]) ** 0.5
+        return s
+
 
 class Cube(Figure):
     sides_count = 12
 
-    def __init__(self, __color, *__sides, filled=True):
-        super().__init__(__color, __sides, filled)
+    def __init__(self, color,  *sides: int, filled: bool = True):
+        super().__init__(color, *sides, filled)
+        if len(sides) == 1:
+            self.__sides = self.sides_count*[i for i in sides]
+        else:
+            self.__sides = [1*self.sides_count]
+
+    def get_sides(self):
+        return [*self.__sides]
+
+    def get_volume(self):
+        return self.__sides[1]**3
 
 
 circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
@@ -96,3 +117,5 @@ print(len(circle1))
 
 # Проверка объёма (куба):
 print(cube1.get_volume())
+
+
